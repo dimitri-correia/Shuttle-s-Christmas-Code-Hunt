fbcae5306522a02bc080e8455a33b681a3871d6e
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2023-11-30 18:54:31 +0100

    improving test

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Set default URL
-URL="http://localhost:8000/"
+URL="http://localhost:8000"
+DAY=0
 
 # Check for options
-while getopts ":r" opt; do
+while getopts ":r:d:" opt; do
   case $opt in
     r)
-      URL="https://cch23-dcorreia.shuttleapp.rs/"
+      URL="https://cch23-dcorreia.shuttleapp.rs"
+      ;;
+    d)
+      DAY=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG. Use '-r' for remote."
@@ -16,11 +19,19 @@ while getopts ":r" opt; do
   esac
 done
 
+echo -e "Test endpoints for day $DAY\n"
 
-###  day -1 test
+# Run commands based on the specified day
+if [ "$DAY" -eq 0 ]; then
+  echo -e "### 1st endpoint"
+  curl -i -X GET "$URL/"
 
-echo -e "### 1st endpoint"
-curl -i -X GET "$URL"
+  echo -e "\n\n### 2nd endpoint"
+  curl -i -X GET "$URL/-1/error"
+elif [ "$DAY" -eq 1 ]; then
+  echo -e "### 1st endpoint"
+  curl -i -X GET "$URL/okok"
 
-echo -e "\n\n### 2nd endpoint"
-curl -i -X GET "$URL-1/error"
+  echo -e "\n\n### 2nd endpoint"
+  curl -i -X GET "$URL/ko"
+fi
