74518220de92b01ed2e001353d7e122d31e6bcb4
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2023-11-30 15:32:42 +0100

    day -1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use axum::http::StatusCode;
 use axum::{routing::get, Router};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn hello_world() -> (StatusCode, &'static str) {
+    (StatusCode::OK, "Hello, world!")
+}
+
+async fn error() -> StatusCode {
+    StatusCode::INTERNAL_SERVER_ERROR
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(error));
 
     Ok(router.into())
 }
