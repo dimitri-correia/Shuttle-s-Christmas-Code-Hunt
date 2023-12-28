f8a136afc40930203b1f825e3e564c78eae60fdc
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2023-12-28 20:13:39 +0100

    day 01 testing

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -19,3 +19,41 @@ async fn cube_the_bits(Path(l_nums): Path<String>) -> (StatusCode, String) {
 
     (StatusCode::OK, format!("{res}"))
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::http::StatusCode;
+    use axum_test::TestServer;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn task1() {
+        let app = get_day_1_router();
+
+        // Run the application for testing.
+        let server = TestServer::new(app).unwrap();
+
+        // Send the request.
+        let response = server.get("/4/8").await;
+
+        response.assert_status(StatusCode::OK);
+
+        response.assert_text(1728.to_string());
+    }
+
+    #[tokio::test]
+    async fn task2() {
+        let app = get_day_1_router();
+
+        // Run the application for testing.
+        let server = TestServer::new(app).unwrap();
+
+        // Send the request.
+        let response = server.get("4/5/8/10").await;
+
+        response.assert_status(StatusCode::OK);
+
+        response.assert_text(27.to_string());
+    }
+}
