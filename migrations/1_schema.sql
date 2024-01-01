8a33e82994e73bf2a1f74785ac6b209488fd6541
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-01-01 17:09:21 +0100

    separate migrations

--- migrations/1_schema.sql
+++ migrations/1_schema.sql
@@ -1,8 +1,1 @@
 DROP TABLE IF EXISTS orders;
-CREATE TABLE orders
-(
-    id        INT PRIMARY KEY,
-    region_id INT,
-    gift_name VARCHAR(50),
-    quantity  INT
-);
---
+++ migrations/2_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE orders
+(
+    id        INT PRIMARY KEY,
+    region_id INT,
+    gift_name VARCHAR(50),
+    quantity  INT
+);
