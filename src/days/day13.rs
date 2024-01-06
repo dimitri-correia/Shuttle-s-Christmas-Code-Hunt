ff0710063761f15f95280455d3ed3119a12878e5
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-01-06 21:48:14 +0100

    day 13 tests

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -65,3 +65,103 @@ async fn get_popular_order(State(db): State<MyState>) -> (StatusCode, Json<Popul
 
     (StatusCode::OK, Json(Popular { popular }))
 }
+
+#[cfg(test)]
+mod tests {
+    use axum_test::TestServer;
+    use serde_json::json;
+    use serial_test::serial;
+    use sqlx::PgPool;
+
+    use super::*;
+
+    async fn setup_test_server() -> TestServer {
+        let pool = PgPool::connect("postgresql://dim:dim@localhost:3269/db")
+            .await
+            .expect("Failed to connect to the database for testing");
+
+        let db = MyState { pool };
+        let app = get_day_13_router(db);
+
+        TestServer::new(app).unwrap()
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn task1() {
+        // Run the application for testing.
+        let server = setup_test_server().await;
+
+        // Send the request.
+        let response = server.get("/sql").await;
+
+        response.assert_status(StatusCode::OK);
+
+        response.assert_text(20231213.to_string());
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn task2() {
+        // Run the application for testing.
+        let server = setup_test_server().await;
+
+        // Send the request.
+        let response = server.post("/reset").await;
+
+        response.assert_status(StatusCode::OK);
+
+        // Send the request.
+        let response = server
+            .post("/orders")
+            .json(
+                &json!([{"id":1,"region_id":2,"gift_name":"Toy Train","quantity":5},
+    {"id":2,"region_id":2,"gift_name":"Doll","quantity":8},
+    {"id":3,"region_id":3,"gift_name":"Action Figure","quantity":12},
+    {"id":4,"region_id":4,"gift_name":"Board Game","quantity":10},
+    {"id":5,"region_id":2,"gift_name":"Teddy Bear","quantity":6},
+    {"id":6,"region_id":3,"gift_name":"Toy Train","quantity":3}]),
+            )
+            .await;
+
+        response.assert_status(StatusCode::OK);
+
+        // Send the request.
+        let response = server.get("/orders/total").await;
+
+        response.assert_status(StatusCode::OK);
+
+        response.assert_json(&json!({"total":44}));
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn task3() {
+        // Run the application for testing.
+        let server = setup_test_server().await;
+
+        // Send the request.
+        let response = server.post("/reset").await;
+
+        response.assert_status(StatusCode::OK);
+
+        // Send the request.
+        let response = server
+            .post("/orders")
+            .json(
+                &json!([{"id":1,"region_id":2,"gift_name":"Toy Train","quantity":5},
+    {"id":2,"region_id":2,"gift_name":"Doll","quantity":8},
+    {"id":3,"region_id":3,"gift_name":"Toy Train","quantity":4}]),
+            )
+            .await;
+
+        response.assert_status(StatusCode::OK);
+
+        // Send the request.
+        let response = server.get("/orders/popular").await;
+
+        response.assert_status(StatusCode::OK);
+
+        response.assert_json(&json!({"popular":"Toy Train"}));
+    }
+}
