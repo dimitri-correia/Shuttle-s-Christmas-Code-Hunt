9918e7d6942da43a62f51eb80c4afb7483ae290f
dimitri correia <125220634+dimitri-correia@users.noreply.github.com>
2024-01-10 22:55:16 +0100

    run shuttle in background

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-cch23-validator --all
+cargo shuttle run &  # Run Cargo shuttle in the background
+wait                # Wait for the background process (Cargo shuttle) to finish
+echo "CC"           # Execute another command after Cargo shuttle is ready
